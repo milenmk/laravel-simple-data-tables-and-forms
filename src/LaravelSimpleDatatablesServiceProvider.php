8ddb1fb3f02a36c73950fa36ae0f8b22c23e0a44
Milen Karaganski <milenmk@gmail.com>
2025-03-21 01:37:28 +0200

    Remove obsolete Livewire components from the package ServiceProvider.
    Signed-off-by: Milen Karaganski <milenmk@gmail.com>

--- src/LaravelSimpleDatatablesServiceProvider.php
+++ src/LaravelSimpleDatatablesServiceProvider.php
@@ -1,24 +1,28 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Milenmk\LaravelSimpleDatatables;
 
 use Illuminate\Support\ServiceProvider;
-use Livewire\Livewire;
-use Milenmk\LaravelSimpleDatatables\Livewire\Components\Checkbox;
-use Milenmk\LaravelSimpleDatatables\Livewire\Components\ToggleSwitch;
 
 class LaravelSimpleDatatablesServiceProvider extends ServiceProvider
 {
     public function boot()
     {
         $this->loadViewsFrom(__DIR__ . '/../resources/views', 'laravel-simple-datatables');
-        Livewire::component('laravel-simple-datatables::toggle-switch', ToggleSwitch::class);
-        Livewire::component('laravel-simple-datatables::checkbox', Checkbox::class);
 
-        $this->publishes([
-            __DIR__ . '/../resources/views/components' => base_path('resources/views/vendor/laravel-simple-datatables/components'),
-            __DIR__ . '/../resources/views/livewire' => base_path('resources/views/vendor/laravel-simple-datatables/livewire'),
-        ], 'laravel-simple-datatables-views');
+        $this->publishes(
+            [
+                __DIR__ . '/../resources/views/components' => base_path(
+                    'resources/views/vendor/laravel-simple-datatables/components',
+                ),
+                __DIR__ . '/../resources/views/livewire' => base_path(
+                    'resources/views/vendor/laravel-simple-datatables/livewire',
+                ),
+            ],
+            'laravel-simple-datatables-views',
+        );
     }
 
     public function register() {}
